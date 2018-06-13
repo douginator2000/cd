58791461a6ac4ac66b2eccbc7ee3457f1da01c7c
douginator2000 <duggus@therebelalliance.gov>
2018-06-13 13:02:17 -0600

    adding tests

--- cd/simple_web_app/tests.py
+++ cd/simple_web_app/tests.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from simple_web_app.models import MountainAnimal
 
 # Create your tests here.
+class MountainAnimalTestCase(TestCase):
+    def setUp(self):
+        MountainAnimal.objects.create(name="mountain_lion", visitation_frequency=1)
+
+    def test_animal_visitation_frequency(self):
+        """Test that mountain animal visitation frequencies are correctly identified"""
+        mountain_lion = MountainAnimal.objects.get(name='mountain_lion')
+        self.assertEqual(mountain_lion.visitation_frequency, 1)
